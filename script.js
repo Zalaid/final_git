d10db3f88000f3e65c40233cc014369a1283b017
Zalaid Butt <zalaidbutt1@gmail.com>
2024-10-09 23:11:27 +0500

    Function to handle contact button click

---
+++ script.js
@@ -0,0 +1,4 @@
+// Function to handle contact button click
+function handleContactClick() {
+    // Alert message when the button is clicked
+    alert("Thank you for your interest! We will get back to you soon.");

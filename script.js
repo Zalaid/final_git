1dfd7daedac6907d588804265015985c088d14b0
Zalaid Butt <zalaidbutt1@gmail.com>
2024-10-09 23:14:00 +0500

    final js

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 function handleContactClick() {
     // Alert message when the button is clicked
     alert("Thank you for your interest! We will get back to you soon.");
+}
 
-    // Attach event listener to the contact button
+// Attach event listener to the contact button
 const contactBtn = document.getElementById("contactBtn");
 if (contactBtn) {
     contactBtn.addEventListener("click", handleContactClick); // Call function on button click
